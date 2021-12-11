37b3f02b43e03b9f8a7336d57f46dec20511dcf7
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-12-10 21:28:31 -0300

    Update exercicio 17

--- exercicio-17/js.js
+++ exercicio-17/js.js
@@ -5,9 +5,9 @@ function tempoTranscorrido(){
 
     let now = new Date()
 
-    let diaNow = now.getDate()
-    let mesNow = now.getMonth() + 1
+    let diaNow = String( now.getDate() ).padStart(2, '0')
+    let mesNow = String(now.getMonth() + 1).padStart(2, '0')
     let anoNow = now.getFullYear()
 
-    alert('Dia: ' + dia - diaNow)
+    alert('Dias: ' + Math.abs(dia - diaNow) + ' Meses: ' + Math.abs(mes - mesNow) + ' Anos: ' + Math.abs(ano - anoNow))
 }
